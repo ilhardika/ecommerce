03d84830dd558fcf5dc3e49f28be56f77336c467
ilhardika <ilhardika67@gmail.com>
2025-05-26 15:19:46 +0700

    feat: integrate Clerk authentication in Header; update SignIn component to use SignInButton

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,13 @@ import FavouriteButton from "./FavouriteButton";
 import CartIcon from "./CartIcon";
 import SignIn from "./SignIn";
 import MobileMenu from "./MobileMenu";
+import { currentUser } from "@clerk/nextjs/server";
+import { UserButton, SignedIn, ClerkLoaded } from "@clerk/nextjs";
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
+  console.log("Current User:", user);
+
   return (
     <header className="bg-white py-5 border-b border-black/20">
       <Container className="flex items-center justify-between">
@@ -18,12 +23,20 @@ const Header = () => {
         </div>
 
         <HeaderMenu />
-        
+
         <div className="flex items-center justify-end gap-5">
           <SearchBar />
           <CartIcon />
           <FavouriteButton />
-          <SignIn />
+          
+          <ClerkLoaded>
+            {/* Jika pengguna sudah masuk (SignedIn), tampilkan tombol UserButton */}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {/* Jika tidak ada user, tampilkan komponen SignIn */}
+            {!user && <SignIn />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,10 +1,13 @@
+import { SignInButton } from "@clerk/nextjs";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <button className="text-sm font-semibold hover:text-darkColor hoverEffect text-lightColor">
-      Login
-    </button>
+    <SignInButton>
+      <button className="text-sm font-semibold hover:text-darkColor hoverEffect text-lightColor">
+        Login
+      </button>
+    </SignInButton>
   );
 };
 
