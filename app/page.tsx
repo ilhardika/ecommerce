95012d64a56bbd30849cb18c7087c2a1d93c0301
ilhardika <ilhardika67@gmail.com>
2025-05-26 16:50:36 +0700

    refactor: clean up Home component by removing commented code and unnecessary elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-// import { Button } from "@/components/ui/button";
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 
 const Home = () => {
   return (
     <Container>
-      {/* <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-        voluptas obcaecati ratione tempore ut, reiciendis velit hic culpa sed
-        ipsum, nostrum neque at consequuntur sequi, doloribus et! Et ipsum, unde
-        accusantium odio veniam, nobis ratione suscipit minima facilis explicabo
-        totam aliquid assumenda quae praesentium excepturi sit sapiente ullam
-        nesciunt ex?
-      </p>
-      <Button>Click</Button> */}
       <HomeBanner />
     </Container>
   );
