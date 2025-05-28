d0527e323e49c1d3c48a5a375604a35b22217e2a
ilhardika <ilhardika67@gmail.com>
2025-05-28 17:46:49 +0700

    feat: refactor RootLayout to remove unnecessary HTML structure and streamline rendering

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -19,15 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
     </ClerkProvider>
   );
 }
