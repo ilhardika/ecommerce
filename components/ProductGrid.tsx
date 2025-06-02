ce042a543f8a8b97b7247a7bfa12adc18a935c71
ilhardika <ilhardika67@gmail.com>
2025-06-02 20:54:23 +0700

    Add Product grid and sanity types, and add no product available component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,17 +1,61 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constant/data";
+import { client } from "@/sanity/lib/client";
+import { AnimatePresence, motion } from "motion/react";
+import { Loader2 } from "lucide-react";
+import NoProductAvailable from "./NoProductAvailable";
+import { Product } from "@/sanity.types";
 
 const ProductGrid = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]); //product type is defined in sanity.types
   const [loading, setLoading] = useState(true);
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+
+  const query = `*[_type == "product" && variant == $variant] | order(name desc){
+  ...,"categories":categories[]->title}`;
+  const params = { variant: selectedTab.toLowerCase() };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        setProducts(response);
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [selectedTab]);
 
   return (
     <div>
       <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+      {loading ? (
+        <div className="flex flex-col items-center justify-center gap-4 py-10 min-h-80 bg-gray-100 w-full mt-10">
+          <div className="flex items-center space-x-2 text-shop_light_green">
+            <Loader2 className="w-5 h-5 animate-spin" />
+            <span>Product is loading</span>
+          </div>
+        </div>
+      ) : products.length ? (
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
+          {products.map((product) => (
+            <AnimatePresence key={product._id}>
+              <motion.div>
+                <div>{product.name}</div>
+              </motion.div>
+            </AnimatePresence>
+          ))}
+        </div>
+      ) : (
+        <NoProductAvailable selectedTab={selectedTab} />
+      )}
     </div>
   );
 };
