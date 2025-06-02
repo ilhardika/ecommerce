c3ca15fbf9678535dd9a59f89a8ad2a235628558
ilhardika <ilhardika67@gmail.com>
2025-06-02 11:15:50 +0700

    feat: enhance HomeTabBar component with props for selected tab and tab selection handling

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -2,14 +2,20 @@ import { productType } from "@/constant/data";
 import Link from "next/link";
 import React from "react";
 
-const HomeTabBar = () => {
+interface Props {
+  selectedTab?: string;
+  onTabSelect?: (tab: string) => void;
+}
+
+const HomeTabBar = ({ selectedTab, onTabSelect }: Props) => {
   return (
     <div className="flex items-center justify-between flex-wrap gap-5">
       <div className="flex items-center gap-3 text-sm font-semibold">
         {productType?.map((item) => (
           <button
+            onClick={() => onTabSelect?.(item?.title)}
             key={item?.title}
-            className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect`}
+            className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/20"}`}
           >
             {item?.title}
           </button>
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -2,15 +2,16 @@
 
 import { useState } from "react";
 import HomeTabBar from "./HomeTabBar";
+import { productType } from "@/constant/data";
 
 const ProductGrid = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [selectedTab, setSelectedTab] = useState("");
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
 
   return (
     <div>
-      <HomeTabBar />
+      <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
     </div>
   );
 };
