31089ce43dbe4287cda0bab56856481d4b1ff1fc
ilhardika <ilhardika67@gmail.com>
2025-06-03 13:56:07 +0700

    feat: add AddToWishlistButton component and integrate it into ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,6 +4,7 @@ import { Flame } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import AddToWishlistButton from "./AddToWishlistButton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -16,8 +17,10 @@ const ProductCard = ({ product }: { product: Product }) => {
             loading="lazy"
             width={700}
             height={700}
+            className={`w-full h-64 object-contain overflow-hidden group-hover:scale-105 transition-transform duration-500 ease-in-out ${product?.stock === 0 && "opacity-0"}`}
           />
         )}
+        <AddToWishlistButton product={product} />
         {(product?.status === "sale" || product?.status === "new") && (
           <p
             className={`absolute top-2 left-2 z-10 text-xs border px-2 rounded-full group-hover:border-shop_light_green hoverEffect group-hover:text-shop_light_green hoverEfffect ${
@@ -30,19 +33,19 @@ const ProductCard = ({ product }: { product: Product }) => {
           </p>
         )}
         {product?.status === "hot" && (
-            <Link
-                href={"/deal"}
-                className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hoverEffect"
-            >
-                <Flame
-                    size={18}
-                    fill="#fb6c08"
-                    className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
-                />
-            </Link>
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hoverEffect"
+          >
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
         )}
       </div>
-      <div className="p-3">Product Detail</div>
+      <div className="p-3"></div>
     </div>
   );
 };
